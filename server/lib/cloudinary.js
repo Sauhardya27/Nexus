16a0eb3c1b6e0761173988a2a667c301a27e01fa
Sauhardya27 <sauhardyachakraborty2643@gmail.com>
2024-12-23 11:41:41 +0530

    Add auth route and auth middleware

--- server/lib/cloudinary.js
+++ server/lib/cloudinary.js
@@ -1,4 +1,7 @@
-const cloudinary = require('cloudinary').v2;
+import { v2 as cloudinary } from 'cloudinary';
+import { config } from 'dotenv';
+
+config();
 
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
@@ -6,4 +9,4 @@ cloudinary.config({
     api_secret: process.env.CLOUD_API_SECRET,
 });
 
-module.exports = cloudinary;
+export default cloudinary;
