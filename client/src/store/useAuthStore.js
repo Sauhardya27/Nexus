dec71b503dcfcedeebda7811a4329b4b9b059cd2
Sauhardya27 <sauhardyachakraborty2643@gmail.com>
2024-12-28 12:19:17 +0530

    Add update-profile section to update the profile image

--- client/src/store/useAuthStore.js
+++ client/src/store/useAuthStore.js
@@ -1,4 +1,3 @@
-import axios from "axios";
 import { create } from "zustand";
 import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
@@ -35,7 +34,7 @@ export const useAuthStore = create((set) => ({
 
 			set({ authUser: response.data });
 			toast.success("Account created successfully");
-			
+
 			get().connectSocket();
 			return response.data;
 		} catch (error) {
@@ -74,6 +73,20 @@ export const useAuthStore = create((set) => ({
 		} catch (error) {
 			console.error("Error in logout:", error);
 			toast.error(error.response?.data?.message || "Failed to logout");
+		}
+	},
+
+	updateProfile: async (data) => {
+		set({ isUpdatingProfile: true });
+		try {
+			const res = await axiosInstance.put("/auth/update-profile", data);
+			set({ authUser: res.data });
+			toast.success("Profile updated successfully");
+		} catch (error) {
+			console.log("error in update profile:", error);
+			toast.error(error.response.data.message);
+		} finally {
+			set({ isUpdatingProfile: false });
 		}
 	},
 }));
