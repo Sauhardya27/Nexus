96dcd94da30483b5a2074cfe40d28bb68b55cc1a
Sauhardya27 <sauhardyachakraborty2643@gmail.com>
2024-12-29 14:15:08 +0530

    Implement chat container enabling users to exchange messages.

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -1,11 +1,14 @@
 import React, { useEffect } from 'react'
 import { useChatStore } from '../store/useChatStore';
+import { useAuthStore } from '../store/useAuthStore';
 import ChatHeader from './ChatHeader';
 import ChatInput from './ChatInput';
 import ChatSkeleton from './skeletons/ChatSkeleton';
+import { formatMessageTime } from '../lib/utils';
 
 const ChatContainer = () => {
   const { chats, getChats, isChatsLoading, selectedUser } = useChatStore();
+  const { authUser } = useAuthStore();
 
   useEffect(() => {
     getChats(selectedUser._id);
@@ -24,7 +27,43 @@ const ChatContainer = () => {
     <div className='flex-1 flex flex-col overflow-auto'>
       <ChatHeader />
 
-      <p>chats...</p>
+      <div className='flex-1 overflow-y-auto p-4 space-y-4'>
+        {chats.map((chat) => (
+          <div
+            key={chat._id}
+            className={`chat ${chat.senderId === authUser._id ? 'chat-end' : 'chat-start'}`}
+          >
+            <div className="chat-image avatar">
+              <div className="size-10 rounded-full border">
+                <img
+                  src={
+                    chat.senderId === authUser._id
+                      ? authUser.avatar || "/avatar.png"
+                      : selectedUser.avatar || "/avatar.png"
+                  }
+                  alt="profile pic"
+                />
+              </div>
+            </div>
+
+            <div className="chat-header mb-1">
+              <time className="text-xs opacity-50 ml-1">
+                {formatMessageTime(chat.createdAt)}
+              </time>
+            </div>
+            <div className="chat-bubble flex flex-col">
+              {chat.image && (
+                <img
+                  src={chat.image}
+                  alt="Attachment"
+                  className="sm:max-w-[200px] rounded-md mb-2"
+                />
+              )}
+              {chat.text && <p>{chat.text}</p>}
+            </div>
+          </div>
+        ))}
+      </div>
 
       <ChatInput />
     </div>
